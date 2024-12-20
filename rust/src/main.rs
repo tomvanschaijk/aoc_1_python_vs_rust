1744c053c675559a49229ce1df47fbf1edc62097
Tom van Schaijk <vanschaijktom@hotmail.com>
2024-12-20 15:00:31 +0100

    Made file-reading async

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::{Context, Result};
-use std::{
+use futures::StreamExt;
+use std::{path::Path, time::Instant};
+use tokio::{
     fs::File,
-    io::{BufRead, BufReader, Lines},
-    path::Path,
-    time::Instant,
+    io::{AsyncBufReadExt, BufReader},
 };
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let files = [
         "./data/input_1k.txt",
         "./data/input_10k.txt",
@@ -19,7 +20,7 @@ fn main() -> Result<()> {
 
         let now = Instant::now();
 
-        match get_sorted_vectors(file) {
+        match get_sorted_vectors(file).await {
             Ok((v1, v2)) => {
                 let distance = compute_distance(&v1, &v2);
                 println!(
@@ -37,32 +38,37 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn get_sorted_vectors(file_path: &str) -> Result<(Vec<i32>, Vec<i32>)> {
+async fn get_sorted_vectors(file_path: &str) -> Result<(Vec<i32>, Vec<i32>)> {
     let lines = read_lines(file_path)
+        .await
         .with_context(|| format!("Failed to read lines from file: {}", file_path))?;
 
-    let (col1, col2): (Vec<i32>, Vec<i32>) = lines
-        .map_while(Result::ok)
-        .filter_map(|line| {
-            let mut nums = line.split_whitespace().map(|x| x.parse::<i32>());
-            match (nums.next(), nums.next()) {
-                (Some(Ok(value1)), Some(Ok(value2))) => Some((value1, value2)),
-                _ => None,
+    let (mut col1, mut col2) = tokio_stream::wrappers::LinesStream::new(lines)
+        .filter_map(|line| async move {
+            match line {
+                Ok(line_str) => {
+                    let mut nums = line_str.split_whitespace().map(|x| x.parse::<i32>());
+                    match (nums.next(), nums.next()) {
+                        (Some(Ok(value1)), Some(Ok(value2))) => Some((value1, value2)),
+                        _ => None,
+                    }
+                }
+                Err(_) => None,
             }
         })
         .fold(
             (Vec::new(), Vec::new()),
-            |(mut col1, mut col2), (v1, v2)| {
+            |(mut col1, mut col2), (v1, v2)| async move {
                 col1.push(v1);
                 col2.push(v2);
                 (col1, col2)
             },
-        );
+        )
+        .await;
 
-    let (mut col1, mut col2) = (col1, col2);
+    // Sort vectors
     col1.sort_unstable();
     col2.sort_unstable();
-
     Ok((col1, col2))
 }
 
@@ -70,11 +76,10 @@ fn compute_distance(v1: &[i32], v2: &[i32]) -> i32 {
     v1.iter().zip(v2).map(|(a, b)| (a - b).abs()).sum()
 }
 
-fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+async fn read_lines<P>(filename: P) -> Result<tokio::io::Lines<BufReader<File>>>
 where
     P: AsRef<Path>,
 {
-    let file = File::open(&filename)
-        .with_context(|| format!("Failed to open file: {:?}", filename.as_ref()))?;
+    let file = File::open(filename).await.context("Failed to open file")?;
     Ok(BufReader::new(file).lines())
 }
