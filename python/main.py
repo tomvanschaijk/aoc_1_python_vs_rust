3a05df229b05f0958addab498af46abf63f484c6
Tom van Schaijk <vanschaijktom@hotmail.com>
2024-12-20 13:54:10 +0100

    Added processing of additional files to see scaling

--- python/main.py
+++ python/main.py
@@ -19,13 +19,26 @@ def get_sorted_vectors(file_path):
 def compute_distance(v1, v2):
     return sum(abs(e1 - e2) for e1, e2 in zip(v1, v2))
 
+def main():
+    files = [
+        "./data/input_1k.txt",
+        "./data/input_10k.txt",
+        "./data/input_100k.txt",
+        "./data/input_1m.txt"
+    ]
 
-start_time = time.time()
+    for file in files:
+        print(f"Processing {file}...")
+        start_time = time.time()
 
-v1, v2 = get_sorted_vectors("./data/input_1k.txt")
-distance = compute_distance(v1, v2)
+        v1, v2 = get_sorted_vectors(file)
+        distance = compute_distance(v1, v2)
 
-end_time = time.time()
-elapsed_time_ms = (end_time - start_time) * 1000
+        end_time = time.time()
+        elapsed_time_ms = (end_time - start_time) * 1000
 
-print(f"The answer is: {distance}, completed in {elapsed_time_ms:.0f}ms")
+        print(f"The answer is: {distance}, completed in {elapsed_time_ms:.0f}ms\n")
+
+
+if __name__ == "__main__":
+    main()
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,52 +1,80 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::{self, BufRead, Lines, Result};
-use std::path::Path;
+use anyhow::{Context, Result};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Lines},
+    path::Path,
+    time::Instant,
+};
 
-fn main() {
-    let now = std::time::Instant::now();
+fn main() -> Result<()> {
+    let files = [
+        "./data/input_1k.txt",
+        "./data/input_10k.txt",
+        "./data/input_100k.txt",
+        "./data/input_1m.txt",
+    ];
 
-    let (v1, v2) = get_sorted_vectors("./data/input_1k.txt");
-    let distance: i32 = compute_distance(&v1, &v2);
+    for file in files.iter() {
+        println!("Processing {}...", file);
 
-    println!("The answer is: {}, completed in {}ms", distance, now.elapsed().as_millis());
-}
+        let now = Instant::now();
 
-fn get_sorted_vectors(file_path: &str) -> (Vec<i32>, Vec<i32>) {
-    let mut column1: Vec<i32> = vec![];
-    let mut column2: Vec<i32> = vec![];
-    match read_lines(file_path) {
-        Ok(lines) => {
-            for line in lines.map_while(Result::ok) {
-                let splitted: Vec<i32> = line
-                    .split_whitespace()
-                    .map(|x| x.parse::<i32>().unwrap())
-                    .collect();
-                column1.push(splitted[0]);
-                column2.push(splitted[1]);
+        match get_sorted_vectors(file) {
+            Ok((v1, v2)) => {
+                let distance = compute_distance(&v1, &v2);
+                println!(
+                    "The answer is: {}, completed in {}ms\n",
+                    distance,
+                    now.elapsed().as_millis()
+                );
+            }
+            Err(e) => {
+                eprintln!("Error processing file '{}': {:?}\n", file, e);
             }
         }
-        Err(_) => panic!("Error reading file"),
     }
 
-    column1.sort();
-    column2.sort();
-
-    (column1, column2)
+    Ok(())
 }
 
-fn compute_distance(sorted_v1: &[i32], sorted_v2: &[i32]) -> i32 {
-    sorted_v1
-        .iter()
-        .zip(sorted_v2)
-        .map(|(c1, c2)| (c1 - c2).abs())
-        .sum()
+fn get_sorted_vectors(file_path: &str) -> Result<(Vec<i32>, Vec<i32>)> {
+    let lines = read_lines(file_path)
+        .with_context(|| format!("Failed to read lines from file: {}", file_path))?;
+
+    let (col1, col2): (Vec<i32>, Vec<i32>) = lines
+        .map_while(Result::ok)
+        .filter_map(|line| {
+            let mut nums = line.split_whitespace().map(|x| x.parse::<i32>());
+            match (nums.next(), nums.next()) {
+                (Some(Ok(value1)), Some(Ok(value2))) => Some((value1, value2)),
+                _ => None,
+            }
+        })
+        .fold(
+            (Vec::new(), Vec::new()),
+            |(mut col1, mut col2), (v1, v2)| {
+                col1.push(v1);
+                col2.push(v2);
+                (col1, col2)
+            },
+        );
+
+    let (mut col1, mut col2) = (col1, col2);
+    col1.sort_unstable();
+    col2.sort_unstable();
+
+    Ok((col1, col2))
+}
+
+fn compute_distance(v1: &[i32], v2: &[i32]) -> i32 {
+    v1.iter().zip(v2).map(|(a, b)| (a - b).abs()).sum()
 }
 
 fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
 where
     P: AsRef<Path>,
 {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    let file = File::open(&filename)
+        .with_context(|| format!("Failed to open file: {:?}", filename.as_ref()))?;
+    Ok(BufReader::new(file).lines())
 }
