278ad8708fe185219ca0a33a694ae7750aa1ad81
Tom van Schaijk <vanschaijktom@hotmail.com>
2024-12-20 23:01:51 +0100

    Added Numba, Numpy and Polars to the fight on Python's side. Still a slight "victory" for Rust!

--- python/main.py
+++ python/main.py
@@ -1,30 +1,31 @@
 import time
+import polars as pl
+import numpy as np
+from numba import jit
 
 def get_sorted_vectors(file_path):
-    col1 = []
-    col2 = []
     try:
-        with open(file_path) as f:
-            for line in f:
-                value1, value2 = map(int, line.split())
-                col1.append(value1)
-                col2.append(value2)
+        df = pl.read_csv(file_path, separator=" ", has_header=False)
+        col1 = df[:, 0].to_numpy()
+        col2 = df[:, 1].to_numpy()
     except ValueError as e:
         raise ValueError(f"Error parsing the file: {e}")
     except FileNotFoundError as e:
         raise FileNotFoundError(f"File not found: {e}")
-    
-    return sorted(col1), sorted(col2)
 
+    return np.sort(col1), np.sort(col2)
+
+# Using Numba's JIT compiler to speed up the distance computation
+@jit(nopython=True, cache=True)
 def compute_distance(v1, v2):
-    return sum(abs(e1 - e2) for e1, e2 in zip(v1, v2))
+    return np.sum(np.abs(v1 - v2))
 
 def main():
     files = [
         "./data/input_1k.txt",
         "./data/input_10k.txt",
         "./data/input_100k.txt",
-        "./data/input_1m.txt"
+        "./data/input_1m.txt",
     ]
 
     for file in files:
@@ -38,7 +39,6 @@ def main():
         elapsed_time_ms = (end_time - start_time) * 1000
 
         print(f"The answer is: {distance}, completed in {elapsed_time_ms:.0f}ms\n")
-
 
 if __name__ == "__main__":
     main()
